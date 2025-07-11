d121367499660c556a2bf3326b65c9a7f60a2787
gustavostallmann <gustavostallmann@gmail.com>
2025-07-11 15:11:19 -0300

    feat: graph implementation

--- src/data_structures/graph.c
+++ src/data_structures/graph.c
@@ -2,40 +2,274 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
 #include "graph.h"
+#include "dictionary.h"
+#include "list.h"
 
-typedef struct {
+typedef struct Graph_st {
+    char *name; 
+    bool directed; 
     int vertex_amount;
     int max_vertex_amount;
+    Dict vertexes_dict; 
 } Graph_st;
 
-typedef struct {
+typedef struct Edge_st {
     Node from;
     Node to; 
-    // char *
-    double meanVel; 
-    double length; 
+    Info info; 
 } Edge_st; 
 
-// typedef void *Edge;
-// typedef void *Info;
+typedef struct Node_st {
+    char *name; 
+    List adjacencies; 
+    Info info; 
+} Node_st; 
 
 static void alloc_error() {
-    printf("ERROR: insuffienct memory for allocation");
+    printf("ERROR: insufficient memory for allocation\n");
     exit(1);
 }
 
-Graph createGraph(int nVert, bool directed) {
+static char* alloc_str(char *str) {
+    assert(str); 
+
+    int str_len = strlen(str); 
+    char *new_str = malloc(str_len * sizeof(char) + 1); 
+    if (new_str == NULL) alloc_error();
+
+    strcpy(new_str, str); 
+    return new_str;
+}
+
+Graph createGraph(int nVert, bool directed, char *nomeGrafo) {
     if (nVert < 0) return NULL;
 
     Graph_st *graph = (Graph_st *) malloc(sizeof(Graph_st));
     if (graph == NULL) alloc_error();
 
-    graph->vertex_amount = 0;
-    graph->max_vertex_amount = nVert;
+    graph->vertexes_dict = new_dict(nVert); 
+    if (graph->vertexes_dict == NULL) {
+        free(graph); 
+        alloc_error();
+    }
+
+    graph->directed = directed; 
+    graph->name = alloc_str(nomeGrafo);
+    graph->max_vertex_amount = nVert; 
+
     return (Graph) graph;
+}
+
+int getMaxNodes(Graph g) {
+    assert(g); 
+    Graph_st *graph = (Graph_st *) g; 
+    return graph->max_vertex_amount;
+}
+
+int getTotalNodes(Graph g) {
+    assert(g); 
+    Graph_st *graph = (Graph_st *) g; 
+    return graph->vertex_amount;
 }
 
 Node addNode(Graph g, char *nome, Info info) {
     assert(g);
+    assert(nome); 
+    assert(info); 
+
+    Graph_st *graph = (Graph_st *) g; 
+
+    int dict_key;
+    for (
+        dict_key = 0; 
+        dict_key < graph->max_vertex_amount && !dict_is_empty(graph->vertexes_dict, dict_key); 
+        dict_key++
+    );
+
+    Node_st *node = (Node_st *) malloc(sizeof(Node_st)); 
+    if (node == NULL) alloc_error(); 
+
+    node->adjacencies = new_list(); 
+    if (node->adjacencies == NULL) {
+        free(node); 
+        alloc_error(); 
+    }
+
+    node->name = alloc_str(nome);
+    if (node->name == NULL) {
+        free(node); 
+        alloc_error(); 
+    }
+
+    node->info = info; 
+    dict_put(graph->vertexes_dict, dict_key, node); 
+
+    graph->vertex_amount++; 
+    return dict_key; 
+}
+
+Node getNode(Graph g, char *nome) {
+    assert(g); 
+
+    Graph_st *graph = (Graph_st *) g; 
+    for (int i = 0; i < dict_get_size(graph->vertexes_dict); i++) {
+        if (dict_is_empty(graph->vertexes_dict, i)) continue; 
+
+        Node_st *node = dict_get(graph->vertexes_dict, i); 
+        if (node == NULL) continue; 
+
+        if (strcmp(node->name, nome) != 0) continue; 
+        return i; 
+    }
+
+    return -1; 
+}
+
+Info getNodeInfo(Graph g, Node node) {
+    assert(g); 
+
+    Graph_st *graph = (Graph_st *) g; 
+    Node_st *found_node = (Node_st *) dict_get(graph->vertexes_dict, (int) node);
+    if (found_node == NULL) return NULL; 
+
+    return found_node->info; 
+}
+
+char* getNodeName(Graph g, Node node) {
+    assert(g); 
+
+    Graph_st *graph = (Graph_st *) g; 
+    Node_st *found_node = (Node_st *) dict_get(graph->vertexes_dict, (int) node);
+    if (found_node == NULL) return NULL; 
+    
+    return found_node->name; 
+}
+
+void setNodeInfo(Graph g, Node nd, Info info) {
+    assert(g);
+    assert(nd);
+    
+    Graph_st *graph = (Graph_st *) g; 
+    Node_st *node = (Node_st *) dict_get(graph->vertexes_dict, (int) nd); 
+    if (node == NULL) return; 
+
+    node->info = info; 
+}
+
+Edge addEdge(Graph g, Node from, Node to, Info info) {
+    assert(g); 
+    assert(from);
+    assert(to); 
+
+    Graph_st *graph = (Graph_st *) g; 
+    Node_st *from_node = (Node_st *) dict_get(graph->vertexes_dict, (int) from); 
+    Node_st *to_node = (Node_st *) dict_get(graph->vertexes_dict, (int) to); 
+    if (from_node == NULL || to_node == NULL) return NULL; 
+    
+    Edge_st *new_edge = (Edge_st *) malloc(sizeof(Edge_st)); 
+    if (new_edge == NULL) alloc_error(); 
+
+    new_edge->from = from; 
+    new_edge->to = to; 
+    new_edge->info = info; 
+
+    list_insert_end(from_node->adjacencies, new_edge); 
+    return new_edge;     
+}
+
+static bool find_edge(ListValue value, void *target) {
+    Edge_st *edge = (Edge_st *) value; 
+    Node *search_node = (Node *) target; 
+
+    return edge->to == *search_node;
+}
+
+Edge getEdge(Graph g, Node from, Node to) {
+    assert(g); 
+
+    Graph_st *graph = (Graph_st *) g; 
+    Node_st *from_node = (Node_st *) dict_get(graph->vertexes_dict, (int) from); 
+    Node_st *to_node = (Node_st *) dict_get(graph->vertexes_dict, (int) to); 
+    if (from_node == NULL || to_node == NULL) return NULL; 
+
+    return (Edge) list_search(from_node->adjacencies, &to, &find_edge); 
+}
+
+Node getFromNode(Graph g, Edge e) {
+    assert(g); 
+    assert(e);
+
+    Graph_st *graph = (Graph_st *) g; 
+    Edge_st *edge = (Edge_st *) e; 
+
+    return edge->from; 
+}
+
+Node getToNode(Graph g, Edge e) {
+    assert(g); 
+    assert(e);
+
+    Graph_st *graph = (Graph_st *) g; 
+    Edge_st *edge = (Edge_st *) e; 
+
+    return edge->to; 
+}
+
+Info getEdgeInfo(Graph g, Edge e) { 
+    assert(g); 
+    assert(e);
+
+    Graph_st *graph = (Graph_st *) g; 
+    Edge_st *edge = (Edge_st *) e; 
+
+    return edge->info; 
+}
+
+void setEdgeInfo(Graph g, Edge e, Info info) {
+    assert(g); 
+    assert(e);
+
+    Graph_st *graph = (Graph_st *) g; 
+    Edge_st *edge = (Edge_st *) e; 
+
+    edge->info = info; 
+}
+
+static bool list_compare_edge(ListValue value, void *target) {
+    Edge_st *node_edge = (Edge_st *) value; 
+    Edge_st *target_edge = (Edge_st *) target; 
+
+    return node_edge == target_edge; 
+}
+
+void removeEdge(Graph g, Edge e) {
+    assert(g); 
+    assert(e);
+
+    Graph_st *graph = (Graph_st *) g; 
+    Edge_st *edge = (Edge_st *) e; 
+
+    Node from_node = edge->from; 
+    Node to_node = edge->to; 
+    Node_st *found_node = (Node_st *) dict_get(graph->vertexes_dict, (int) from_node);
+    list_remove_value(found_node->adjacencies, e, &list_compare_edge);   
+}
+
+static bool list_compare_adjacency(ListValue value, void *target) {
+    Edge_st *edge = (Edge_st *) value; 
+    Node *to_node = (Node *) target; 
+    return edge->to == *to_node; 
+}
+
+bool isAdjacent(Graph g, Node from, Node to) {
+    assert(g); 
+
+    Graph_st *graph = (Graph_st *) g; 
+    Node_st *from_node = (Node_st *) dict_get(graph->vertexes_dict, (int) from);
+    Node_st *to_node = (Node_st *) dict_get(graph->vertexes_dict, (int) to);
+
+    if (from_node == NULL || to_node == NULL) return false;
+    return list_search(from_node->adjacencies, to_node, list_compare_adjacency);
 }
